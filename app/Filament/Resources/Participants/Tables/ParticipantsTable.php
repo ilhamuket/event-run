ba71c30b2d08b2135926f33d201767da6d8b3458
ilhamuket <ilhamgallagher633@gmail.com>
2026-02-15 18:56:44 +0700

    fix: filter

--- app/Filament/Resources/Participants/Tables/ParticipantsTable.php
+++ app/Filament/Resources/Participants/Tables/ParticipantsTable.php
@@ -8,6 +8,8 @@ use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 
 class ParticipantsTable
@@ -40,7 +42,8 @@ class ParticipantsTable
                     ->searchable(),
                 TextColumn::make('regency')
                     ->searchable(),
-                TextColumn::make('category')
+                TextColumn::make('category.name')
+                    ->label('Kategori')
                     ->searchable(),
                 TextColumn::make('city')
                     ->searchable(),
@@ -73,7 +76,48 @@ class ParticipantsTable
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                SelectFilter::make('event')
+                    ->relationship('event', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->label('Event'),
+
+                SelectFilter::make('event_category_id')
+                    ->relationship('category', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->label('Kategori'),
+
+                SelectFilter::make('gender')
+                    ->options([
+                        'M' => 'Laki-laki',
+                        'F' => 'Perempuan',
+                    ])
+                    ->label('Jenis Kelamin'),
+
+                SelectFilter::make('jersey_size')
+                    ->options([
+                        'S' => 'S',
+                        'M' => 'M',
+                        'L' => 'L',
+                        'XL' => 'XL',
+                        'XXL' => 'XXL',
+                    ])
+                    ->label('Ukuran Jersey'),
+
+                SelectFilter::make('city')
+                    ->options(fn () => \App\Models\Participant::query()
+                        ->whereNotNull('city')
+                        ->distinct()
+                        ->pluck('city', 'city')
+                        ->toArray())
+                    ->searchable()
+                    ->label('Kota'),
+
+                TernaryFilter::make('has_comorbid')
+                    ->label('Komorbid')
+                    ->trueLabel('Ya')
+                    ->falseLabel('Tidak'),
             ])
             ->recordActions([
                 ViewAction::make(),
