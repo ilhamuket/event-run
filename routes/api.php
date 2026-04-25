e4b0e285ecf277d8bf3b6ac28038c1d0307bf813
Muhammad Ilham <ilhamgallagher633@gmail.com>
2026-04-26 03:07:52 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TripayCallbackController;
 use App\Http\Controllers\RfidTimingController;
+use App\Http\Controllers\TripayCallbackController;
+use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
