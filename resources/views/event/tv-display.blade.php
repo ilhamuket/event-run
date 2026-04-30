a6a8f40fa4dcc69bb9a5606a3723b928919feeec
Muhammad Ilham <ilhamgallagher633@gmail.com>
2026-04-30 21:15:55 +0800

    fixxx

--- resources/views/event/tv-display.blade.php
+++ resources/views/event/tv-display.blade.php
@@ -566,7 +566,7 @@
             String(now.getMinutes()).padStart(2,'0') + ':' +
             String(now.getSeconds()).padStart(2,'0');
     }
-    updateClock();
+   // updateClock();
     setInterval(updateClock, 1000);
 
     function fitName(text) {
